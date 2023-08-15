dd21a584baafcb70e784ea6fcc812fd918f2b71f
= <quoclongdng@gmail.com>
2023-08-15 19:20:15 +0700

    newcode

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"; // cài vue-router: npm install vue-router@next --save
 
 const routes = [
-    {
-        path : '/',
-        component: ()=>import('../components/test/index.vue')
-    },
-    {
-        path : '/test',
-        component: ()=>import('../components/Hello/index.vue')
-    },
+    
 ]
 
 const router = createRouter({
