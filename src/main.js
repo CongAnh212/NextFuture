f18aba91102a7d9968668d768724aaf95735de39
LE CONG ANH <conganh2122003@gmail.com>
2023-11-01 18:12:23 +0700

    hello

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Default from './Layout/Wrapper/index.vue'
+import Default from './Layout/Wrapper/client/index.vue'
 const app = createApp(App)
 
 app.use(router)
